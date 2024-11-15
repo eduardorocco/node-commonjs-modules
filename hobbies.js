363f3de9627987d3252b2e5450adf7ffda903142
eduardorocco <eduardo_rocco@hotmail.it>
2024-11-15 16:10:12 +0100

    ADD second function

---
+++ hobbies.js
@@ -0,0 +1,12 @@
+function hobbies(hobbyOne, hobbyTwo, hobbyThree) {
+
+    return {
+        hobbies: [
+            hobbyOne,
+            hobbyTwo,
+            hobbyThree
+        ]
+    }
+}
+
+// console.log(hobbies('lego', 'videogames', 'disegno'));
